fdc2bddb1d83de04556139571a7a7c64c2689480
maxxless <32952453+maxxless@users.noreply.github.com>
2018-06-10 21:40:08 +0300

    Update Main.java

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -2,37 +2,37 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Helicopter MI_1 = new Helicopter();
-		Helicopter KT_112 = new Helicopter("UA", "KT-112", 250.0, 2000.2);
-		Helicopter Skyline_800 = new Helicopter("USA", "Skyline-800", 315.3, 3200.6, 1325.5);
+		Helicopter mI1 = new Helicopter();
+		Helicopter kT112 = new Helicopter("UA", "KT-112", 250.0, 2000.2);
+		Helicopter skyline800 = new Helicopter("USA", "Skyline-800", 315.3, 3200.6, 1325.5);
 		
-		MI_1.setModel("MI-1");
-		MI_1.setWeight(155.3);
-		MI_1.setSpeed(2420.5);
+		mI1.setModel("MI-1");
+		mI1.setWeight(155.3);
+		mI1.setSpeed(2420.5);
 		
-		System.out.println(MI_1.toString());
-		System.out.println(KT_112.toString());
-		System.out.println(Skyline_800.toString());
+		System.out.println(mI1.toString());
+		System.out.println(kT112.toString());
+		System.out.println(skyline800.toString());
 		
 		Helicopter.printStaticSum();
 		
-		MI_1.printSum();
-		KT_112.printSum();
-		Skyline_800.printSum();
+		mI1.printSum();
+		kT112.printSum();
+		skyline800.printSum();
 		
-		MI_1.resetValue("USSR", "MI-1", 159.9, 1500.2, 1100.2);
-		KT_112.resetValue("UA", "KT-112", 250.0, 2000.2, 1300.5);
-		Skyline_800.resetValue("USA", "Skyline-800", 315.3, 3200.6, 1325.5);
+		mI1.resetValue("USSR", "MI-1", 159.9, 1500.2, 1100.2);
+		kT112.resetValue("UA", "KT-112", 250.0, 2000.2, 1300.5);
+		skyline800.resetValue("USA", "Skyline-800", 315.3, 3200.6, 1325.5);
 				
-		System.out.println(MI_1.toString());
-		System.out.println(KT_112.toString());
-		System.out.println(Skyline_800.toString());
+		System.out.println(mI1.toString());
+		System.out.println(kT112.toString());
+		System.out.println(skyline800.toString());
 		
 		Helicopter.printStaticSum();
 		
-		MI_1.printSum();
-		KT_112.printSum();
-		Skyline_800.printSum();
+		mI1.printSum();
+		kT112.printSum();
+		skyline800.printSum();
 	}
 
 }
